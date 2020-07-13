d0573efa42bb5ec988173e8d8a86bae514d1ad35
Ade Fikri Malihuddin <ade.fm97@gmail.com>
2020-07-13 16:02:28 +0700

    Part 4 Complete

--- scripts/WeaponKnife.cs
+++ scripts/WeaponKnife.cs
@@ -1,72 +1,77 @@
-﻿using Godot;
+using Godot;
 
 public class WeaponKnife : AbstractWeapon
 {
-    public override byte DAMAGE => 40;
-    public override string IDLE_ANIM_NAME => "Knife_idle";
-    public override string FIRE_ANIM_NAME => "Knife_fire";
+	public override byte DAMAGE => 40;
+	public override string IDLE_ANIM_NAME => "Knife_idle";
+	public override string FIRE_ANIM_NAME => "Knife_fire";
 
-    public override byte AMMO_IN_MAG => 1;
+	public override byte AMMO_IN_MAG => 1;
 
-    public override bool CAN_RELOAD => false;
+	public override bool CAN_RELOAD => false;
 
-    public override bool CAN_REFILL => false;
+	public override bool CAN_REFILL => false;
 
-    public override string RELOAD_ANIM_NAME => "";
+	public override string RELOAD_ANIM_NAME => "";
 
-    public WeaponKnife()
-    {
-        AmmoInWeapon = 1;
-        SpareAmmo = 1;
-    }
+	public WeaponKnife()
+	{
+		AmmoInWeapon = 1;
+		SpareAmmo = 1;
+	}
 
-    public override void FireWeapon()
-    {
-        Area area = GetNode<Area>("Area");
-        var bodies = area.GetOverlappingBodies();
+	public override void FireWeapon()
+	{
+		Area area = GetNode<Area>("Area");
+		var bodies = area.GetOverlappingBodies();
 
-        foreach(var body in bodies)
-        {
-            if (body == PlayerNode) continue;
+		foreach(var body in bodies)
+		{
+			if (body == PlayerNode) continue;
 
-            if ((body as Spatial).HasMethod("BulletHit"))
-            {
-                var tmpBody = body as RigidBodyHitTest;
-                tmpBody.BulletHit(DAMAGE, area.GlobalTransform);
-            }
-        }
-    }
+			if ((body as Spatial).HasMethod("BulletHit"))
+			{
+				if (body is RigidBodyHitTest)
+				{
+					var tmpBody = body as RigidBodyHitTest;
+					tmpBody.BulletHit(DAMAGE, area.GlobalTransform);
+				}
+				else if (body is Target aTarget)
+					aTarget.BulletHit(DAMAGE, area.GlobalTransform);
+			}
+		}
+	}
 
-    public override bool EquipWeapon()
-    {
-        if (PlayerNode.AnimationManager.CurrentState == IDLE_ANIM_NAME)
-        {
-            IsWeaponEnabled = true;
-            return true;
-        }
+	public override bool EquipWeapon()
+	{
+		if (PlayerNode.AnimationManager.CurrentState == IDLE_ANIM_NAME)
+		{
+			IsWeaponEnabled = true;
+			return true;
+		}
 
-        if (PlayerNode.AnimationManager.CurrentState == "Idle_unarmed")
-            PlayerNode.AnimationManager.SetAnimation("Knife_equip");
+		if (PlayerNode.AnimationManager.CurrentState == "Idle_unarmed")
+			PlayerNode.AnimationManager.SetAnimation("Knife_equip");
 
-        return false;
-    }
+		return false;
+	}
 
-    public override bool UnequipWeapon()
-    {
-        if (PlayerNode.AnimationManager.CurrentState == IDLE_ANIM_NAME)
-            PlayerNode.AnimationManager.SetAnimation("Knife_unequip");
+	public override bool UnequipWeapon()
+	{
+		if (PlayerNode.AnimationManager.CurrentState == IDLE_ANIM_NAME)
+			PlayerNode.AnimationManager.SetAnimation("Knife_unequip");
 
-        if (PlayerNode.AnimationManager.CurrentState == "Idle_unarmed")
-        {
-            IsWeaponEnabled = false;
-            return true;
-        }
+		if (PlayerNode.AnimationManager.CurrentState == "Idle_unarmed")
+		{
+			IsWeaponEnabled = false;
+			return true;
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    public override bool ReloadWeapon()
-    {
-        return false;
-    }
+	public override bool ReloadWeapon()
+	{
+		return false;
+	}
 }
